b90bb3274a3683f063bc16e8dae37d96be6408a2
pyaephyo11979 <www.pyaephyoehlaing16@gmail.com>
2023-11-05 05:37:16 +0630

    Update

---
+++ script.js
@@ -0,0 +1,40 @@
+let year=document.querySelector("#selYear");
+let bookDisplay=document.querySelector("#books");
+let btn=document.querySelector("#searchBtn");
+let beforeReqPage=document.querySelector("#beforeReq");
+let afterReqPage=document.querySelector("#afterReq");
+let backBtn=document.querySelector("#backBtn");
+afterReqPage.style.display="none";
+btn.addEventListener("click",()=>{
+    let selectedYear=year.options[year.selectedIndex].value;
+    bookDisplay.innerHTML = ''; // clear the book display
+    afterReqPage.style.display="block";
+    fetch(`https://ucsbooksapi.onrender.com/books/year/${selectedYear}`)
+    .then(res=>res.json())
+    .then((data)=>{
+        let books=data;
+        books.forEach(book=>{
+            bookDisplay.innerHTML+=`
+            <div class="card book col-12 col-lg-4 m-1">
+            <div class="card-body">
+            <h4>${book.name}</h4>
+            </div>
+            <div class="card-body">
+            <h5>${book.year}</h5>
+            </div>
+            <div class="card-footer">
+            <a href="${book.url}"  class="btn btn-primary">View</a>
+            </div>
+            </div>
+            `
+        })
+        if(books.length==0){
+            bookDisplay.innerHTML="No Books Found"
+        }
+    })
+    beforeReqPage.style.display="none";
+})
+backBtn.addEventListener("click",()=>{
+    beforeReqPage.style.display="block";
+    afterReqPage.style.display="none";
+})
